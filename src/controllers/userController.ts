01cb6ce507ef8553e9ca7fd57b69ea2982dbfd6c
Andriy Sak <andreyysak17@gmail.com>
2026-01-16 12:58:51 +0200

    feat: add update user city to controller & route

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -26,3 +26,19 @@ export const getMe = async (req: Request, res: Response) => {
     res.status(500).json({ error: 'Failed to fetch profile' });
   }
 };
+
+export const updateUserCity = async (req: Request, res: Response) => {
+  try {
+    const userId = Number((req as any).user.userId);
+    const { city } = req.body;
+
+    const updatedUser = await prisma.user.update({
+      where: { user_id: userId },
+      data: { city }
+    });
+
+    res.json({ message: 'City updated successfully', city: updatedUser.city });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update city' });
+  }
+};
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -5,5 +5,6 @@ import { authMiddleware } from '../middlewares/authMiddleware';
 const router = Router();
 
 router.get('/me', authMiddleware, userController.getMe);
+router.get('/me/updateCity', authMiddleware, userController.updateUserCity)
 
 export default router;
